d1c26dcfb58aca078861866de81ab9a8ea0245de
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-12-14 20:38:21 +0530

    prisma schema

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,1 +1,14 @@
 import express from "express";
+
+const app = express();
+
+// todo password logic here
+
+app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+
+  // store in db a new trigger
+
+  // push it on to a queue (kafka / redis)
+});
