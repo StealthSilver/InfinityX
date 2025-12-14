669b9f490b1700bcd60b349519a92591a80791c4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-12-14 22:15:25 +0530

    client setup

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,14 +1,36 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
+app.use(express.json());
 
-// todo password logic here
-
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
+// password logic
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
+  const body = req.body;
 
   // store in db a new trigger
+  await client.$transaction(async (tx) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
 
-  // push it on to a queue (kafka / redis)
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+  res.json({
+    message: "Webhook received",
+  });
 });
+
+app.listen(3002);
